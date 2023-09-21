f864b25a0300a006c1189ec5db8920ead8c207c1
U27608 <rishabh.joshi@centrisag.ch>
2023-09-21 10:27:33 +0200

    create project

---
+++ sverwaltung/sverwaltung/src/main/java/ch/rishabh/sverwaltung/SverwaltungBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.rishabh.sverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SverwaltungBackendApplication {g
+
+	public static void main(String[] args) {
+		SpringApplication.run(SverwaltungBackendApplication.class, args);
+	}
+
+}
---
+++ sverwaltung/sverwaltung/src/test/java/ch/rishabh/sverwaltung/SverwaltungBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.rishabh.sverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SverwaltungBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
